5bb868a46df9345ab4828524b6a63781ee8fd45d
Shivam Mishra <shivammishra10m@gmail.com>
2026-08-04 12:26:28 +0530

    Uploading Learning code-1

---
+++ track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST02/Learn/Practice_1.java
@@ -0,0 +1,15 @@
+
+public class Practice_1 {
+
+    public static void main(String[] args) {
+        int num = 25;
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
