08cf637e95dcab773d203b92e41b5920a6a1382c
Shivam Mishra <shivammishra10m@gmail.com>
2026-08-06 23:32:30 +0530

    Learning Labeled Loop in JAVA

---
+++ track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST02/Learn/Labeled_Loop.java
@@ -0,0 +1,17 @@
+
+public class Labeled_Loop {
+
+    public static void main(String[] args) {
+        System.out.println("Labelled loops in Java");
+        outer:
+        for (int i = 1; i <= 3; i++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2) {
+                    break outer;
+                }
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
