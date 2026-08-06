cdd1ac0d41508817ec1cdf18fc7759b033fb3210
Shivam Mishra <shivammishra10m@gmail.com>
2026-08-06 12:49:31 +0530

    Learning Nested Loop with Jump Controls(break, continue)

---
+++ track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST02/Learn/Learn_7.java
@@ -0,0 +1,14 @@
+
+public class Learn_7 {
+
+    public static void main(String[] args) {
+        System.out.println("break Jump Control in JAVA");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 6) {
+                break;
+            }
+        }
+    }
+}
---
+++ track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST02/Learn/Learn_8.java
@@ -0,0 +1,14 @@
+
+public class Learn_8 {
+
+    public static void main(String[] args) {
+        System.out.println("continue Jump Control in JAVA");
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
