5c5de7d366bcc1c744c6cd21f76d368b38ead936
Shivam Mishra <shivammishra10m@gmail.com>
2026-08-12 12:56:45 +0530

    Learning Method Overloading Or Virtual Polymorphism in Java

---
+++ track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST05/Method_Overloading.java
@@ -0,0 +1,46 @@
+
+class Method_Overloading {
+
+    public static void main(String[] args) {
+        Addition al = new Addition();
+
+        System.out.println(al.add(10, 20));
+        System.out.println(al.add(10, 30.5));
+        System.out.println(al.add(21.5, 20));
+        System.out.println(al.add(21.5, 21.5));
+        System.out.println(al.add(20, 20, 21.5));
+        System.out.println(al.add(20, 11.5, 11.5));
+
+    }
+}
+
+class Addition {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(int a, double b) {
+        return a + b;
+    }
+
+    double add(double a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(int a, double b, double c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b, double c) {
+        return a + b + c;
+    }
+}
