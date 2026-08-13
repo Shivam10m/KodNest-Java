a4f0f2003cabfc56c870143b8dc3869d177138dc
Shivam Mishra <shivammishra10m@gmail.com>
2026-08-13 20:05:05 +0530

    Adding additional info in Method_Overloading.java file about method overloading

--- track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST05/Method_Overloading.java
+++ track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST05/Method_Overloading.java
@@ -3,7 +3,10 @@ class Method_Overloading {
 
     public static void main(String[] args) {
         Addition al = new Addition();
-
+        System.out.println("Method overloading in java happens if multiple methods have same name and follows any one of the below conditions: ");
+        System.out.println("1. Number of parameters should be different");
+        System.out.println("2. Datatype of the parameters should be different");
+        System.out.println("3. Order of parameters should be different\n");
         System.out.println(al.add(10, 20));
         System.out.println(al.add(10, 30.5));
         System.out.println(al.add(21.5, 20));
