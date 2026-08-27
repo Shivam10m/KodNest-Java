a846f157fe3c308b1359c7c8f81b6b576a489e5a
Shivam Mishra <shivammishra10m@gmail.com>
2026-08-27 15:29:10 +0530

    Populating and printing 2D Array elemnts in Java

--- track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST08-2D_Arrays/Learn/Learn1.java
+++ track/Java_Course/M02-Intro_to_Java_and_Basics/T01/ST08-2D_Arrays/Learn/Learn1.java
@@ -6,5 +6,27 @@ public class Learn1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Learning 2D arrays in java");
+
+        System.out.print("Enter Number of rows: ");
+        int rows = scan.nextInt();
+        System.out.print("Enter Number of columns: ");
+        int cols = scan.nextInt();
+
+        int[][] arr = new int[rows][cols];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print("Enter row " + (i + 1) + " column " + (j + 1) + " element: ");
+                arr[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println("Array Elements:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
