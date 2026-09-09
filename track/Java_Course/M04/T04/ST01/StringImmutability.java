177b96054dc7864a4ef09d6aec08af8218300836
Shivam Mishra <shivammishra10m@gmail.com>
2026-09-09 16:55:35 +0530

    Learning how string is immutable after apply methods on it

---
+++ track/Java_Course/M04/T04/ST01/StringImmutability.java
@@ -0,0 +1,11 @@
+
+public class StringImmutability {
+
+    public static void main(String[] args) {
+        String str = "Programming";
+        String upperStr = str.toUpperCase();
+        System.out.println(upperStr);
+        System.out.println(str);
+
+    }
+}
